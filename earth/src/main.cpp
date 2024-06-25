c9dd6863fcba3679553fe271b58b46d93181891b
Kashiuchi Sotaro <sotaro0416@gmail.com>
2024-06-25 19:12:23 +0900

    position_publish

--- earth/src/main.cpp
+++ earth/src/main.cpp
@@ -32,7 +32,7 @@
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
 //micro-ROS関連で必要となる変数を宣言しておく
-rcl_publisher_t publisher;
+rcl_publisher_t position_publisher;
 std_msgs__msg__Int32 msg;
 rclc_executor_t executor;
 rclc_support_t support;
@@ -48,14 +48,7 @@ void error_loop(){
   }
 }
 
-// コールバック関数
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.data++;
-  }
-}
+void position_publish();
 
 HardwareSerial PCSerial(0);
 HardwareSerial IM920Serial(2);
@@ -109,25 +102,19 @@ void setup() {
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
   // nodeの作成
-  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+  RCCHECK(rclc_node_init_default(&node, "ESP_node", "", &support));
 
   // publisherの作成
   RCCHECK(rclc_publisher_init_default(
-    &publisher,
+    &position_publisher,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "micro_ros_platformio_node_publisher"));
+    "position_publisher"));
 
-  // timerの作成
-  const unsigned int timer_timeout = 1000;
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
-
-  // executorの作成
+  // executorの初期化
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  
+  // timerをexecutorに追加
   RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
   msg.data = 0;
@@ -138,8 +125,8 @@ void setup() {
 }
 
 void loop() {
-  delay(100);
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  delay(10);
+  position_publish();
   
   // Redirect from PCSerial to IM920sL
   while(PCSerial.available()){
@@ -151,4 +138,10 @@ void loop() {
   while(IM920Serial.available()){
     DEBUG_PRINTLN(IM920Serial.readStringUntil('\n'));
   }
+}
+
+// position_publish
+void position_publish() {
+  msg.data++;
+  RCSOFTCHECK(rcl_publish(&position_publisher, &msg, NULL));
 }
