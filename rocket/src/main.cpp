ab1c904d87cbd2d57edbf708d5a120eebd37a32f
Kashiuchi Sotaro <sotaro0416@gmail.com>
2024-08-13 21:41:43 +0900

    rocket

--- rocket/src/main.cpp
+++ rocket/src/main.cpp
@@ -7,10 +7,7 @@
 #include <rclc/executor.h>
 #include <WiFi.h>
 #include <SoftwareSerial.h>
-#include <std_msgs/msg/int32.h>
 #include <ESP32Ping.h>
-#include <custom_message/msg/position.h>
-#include <custom_message/msg/euler.h>
 #include <Adafruit_BNO08x.h>
 #include <Wire.h>
 #include <TinyGPS++.h>
@@ -34,30 +31,12 @@
 #define BNO08X_INT 4
 #define BNO08X_RESET -1
 
-//エラーハンドリング用のマクロ
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+typedef struct {
+  float yaw;
+  float pitch;
+  float roll;
+} euler_t;
 
-//micro-ROS関連で必要となる変数を宣言しておく
-rcl_publisher_t position_publisher;
-rcl_publisher_t euler_publisher;
-custom_message__msg__Position position_msg;
-custom_message__msg__Euler euler_msg;
-rclc_executor_t executor;
-rclc_support_t support;
-rcl_allocator_t allocator;
-rcl_node_t node;
-rcl_timer_t timer;
-
-//micro-ROS関連でエラーが出るとマイコン上の青色LEDが点滅するように設定している
-void error_loop(){
-  while(1){
-    digitalWrite(LED_BLUE, !digitalRead(LED_BLUE));
-    delay(100);
-  }
-}
-
-void position_publish();
 void quaternionToEuler(float qr, float qi, float qj, float qk);
 void setReports();
 void displayInfo(); 
@@ -68,6 +47,7 @@ HardwareSerial GpsSerial(1);
 Adafruit_BNO08x bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
 TinyGPSPlus gps;
+euler_t ypr;
 
 void setup() {
   // PCSerial
@@ -83,25 +63,6 @@ void setup() {
   pinMode(LED_BLUE, OUTPUT);
   analogSetAttenuation(ADC_0db);
 
-  DEBUG_PRINTLN("");
-  DEBUG_PRINTLN("[Starting]:Setting");
-  // WiFi setting
-  WiFi.config(
-    IPAddress(10, 42, 0, 101),      // IPaddress
-    IPAddress(10, 42, 0, 1),        // Gateway
-    IPAddress(255, 255, 255, 255)   // Subnet
-  );
-  set_microros_wifi_transports(
-    "ESP32",
-    "Password12345678",
-    IPAddress(10, 42, 0, 1),
-    2000
-  );
-  DEBUG_PRINTLN("[Finished]:WiFi Connected!!");
-  DEBUG_PRINT("[Info]:Local IPaddress is ");
-  DEBUG_PRINTLN(WiFi.localIP());
-  delay(2000);
-
   // Ping
   if(Ping.ping(IPAddress(10, 42, 0, 1))){
 		DEBUG_PRINTLN("[Success]:Ping to 10.42.0.1");
@@ -109,57 +70,20 @@ void setup() {
 		DEBUG_PRINTLN("[Error]:Ping to 10.42.0.1");
   }
 
-  /*
-   * micro-ROS
-   */
-  // micro-ROS用のメモリを確保
-  allocator = rcl_get_default_allocator();
-
-  // rclcの初期化
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
-  // nodeの作成
-  RCCHECK(rclc_node_init_default(&node, "ESP_node", "", &support));
-
-  // publisherの作成
-  rclc_publisher_init_default(
-    &position_publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(custom_message, msg, Position),
-    "position_publisher");
-
-	rclc_publisher_init_default(
-			&euler_publisher,
-			&node,
-			ROSIDL_GET_MSG_TYPE_SUPPORT(custom_message, msg, Euler),
-			"euler_publisher");
-
-  // executorの初期化
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  
-  // timerをexecutorに追加
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
-
-  position_msg.x = 0;
-  position_msg.y = 0;
-  position_msg.z = 0;
-	DEBUG_PRINTLN("[Finished]:Micro-ROS Setting");
-
-  // Wire.begin(21, 22); // SDAピンは21、SCLピンは22
-  // if (!bno08x.begin_I2C()) {
-  // 	DEBUG_PRINTLN("BNO08xチップが見つかりませんでした");
-  //   while (1) { delay(10); }
-  // }
-  // setReports();
-  // delay(100);
-  // DEBUG_PRINTLN("[Finished]:setting BN008x");
+  Wire.begin(21, 22); // SDAピンは21、SCLピンは22
+  if (!bno08x.begin_I2C()) {
+  	DEBUG_PRINTLN("BNO08xチップが見つかりませんでした");
+    while (1) { delay(10); }
+  }
+  setReports();
+  delay(100);
+  DEBUG_PRINTLN("[Finished]:setting BN008x");
 
   digitalWrite(LED_BLUE, HIGH);
 }
 
 void loop() {
   delay(10);
-  position_publish();
   
   // Redirect from PCSerial to IM920sL
   while(PCSerial.available()){
@@ -175,17 +99,16 @@ void loop() {
     setReports();
   }
 
-  // if (bno08x.getSensorEvent(&sensorValue)) {
-  //   switch (sensorValue.sensorId) {
-  //     case SH2_ROTATION_VECTOR:
-	// 			quaternionToEuler(sensorValue.un.rotationVector.real, sensorValue.un.rotationVector.i, sensorValue.un.rotationVector.j, sensorValue.un.rotationVector.k);
-  //       DEBUG_PRINT("ヨー: "); DEBUG_PRINT(euler_msg.yaw);
-  //       DEBUG_PRINT(", ピッチ: "); DEBUG_PRINT(euler_msg.pitch);
-  //       DEBUG_PRINT(", ロール: "); DEBUG_PRINTLN(euler_msg.roll);
-	// 			RCSOFTCHECK(rcl_publish(&euler_publisher, &euler_msg, NULL));
-  //       break;
-  //   }
-  // }
+  if (bno08x.getSensorEvent(&sensorValue)) {
+    switch (sensorValue.sensorId) {
+      case SH2_ROTATION_VECTOR:
+				quaternionToEuler(sensorValue.un.rotationVector.real, sensorValue.un.rotationVector.i, sensorValue.un.rotationVector.j, sensorValue.un.rotationVector.k);
+        DEBUG_PRINT("ヨー: "); DEBUG_PRINT(ypr.yaw);
+        DEBUG_PRINT(", ピッチ: "); DEBUG_PRINT(ypr.pitch);
+        DEBUG_PRINT(", ロール: "); DEBUG_PRINTLN(ypr.roll);
+        break;
+    }
+  }
 
   while (GpsSerial.available() > 0) {
     char c = GpsSerial.read();
@@ -193,14 +116,6 @@ void loop() {
       displayInfo();
     }
   }
-}
-
-// position_publish
-void position_publish() {
-  position_msg.x++;
-  position_msg.y--;
-  position_msg.z=position_msg.x*position_msg.y;
-  RCSOFTCHECK(rcl_publish(&position_publisher, &position_msg, NULL));
 }
 
 void setReports() {
@@ -215,9 +130,9 @@ void quaternionToEuler(float qr, float qi, float qj, float qk) {
   float sqj = sq(qj);
   float sqk = sq(qk);
 
-  euler_msg.yaw = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr)) * RAD_TO_DEG;
-  euler_msg.pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr)) * RAD_TO_DEG;
-  euler_msg.roll = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr)) * RAD_TO_DEG;
+  ypr.yaw = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr)) * RAD_TO_DEG;
+  ypr.pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr)) * RAD_TO_DEG;
+  ypr.roll = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr)) * RAD_TO_DEG;
 }
 
 void displayInfo()
